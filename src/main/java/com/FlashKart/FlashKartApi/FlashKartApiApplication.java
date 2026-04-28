264f63d654770dec7d3b0e5018ca35f55d18e8ae
Adityanv <vijayadityan209@gmail.com>
2026-04-28 12:33:13 +0530

    Initial commit

---
+++ src/main/java/com/FlashKart/FlashKartApi/FlashKartApiApplication.java
@@ -0,0 +1,13 @@
+package com.FlashKart.FlashKartApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashKartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashKartApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlashKart/FlashKartApi/FlashKartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlashKart.FlashKartApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashKartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
